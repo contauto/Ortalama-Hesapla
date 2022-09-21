6b16deff80ad42d0a30a82c99658a23b929db94c
Kağan Berke Maktav <76818792+contauto@users.noreply.github.com>
2022-09-21 21:56:37 +0300

    Update aboutMe.component.ts

--- src/app/components/aboutMe/aboutMe.component.ts
+++ src/app/components/aboutMe/aboutMe.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutMe.component.css']
 })
 export class AboutMeComponent implements OnInit {
-
+year:number=Number((new Date()).getFullYear())
   constructor() { }
 
   ngOnInit() {
